e0c2108cef6b481487bfffd827b5d14d6f9e268c
lumkonjenjess-sudo <lumkonjenjess@gmail.com>
2026-07-22 22:11:31 +0200

    Update page.js
    Connects the KuroKage shop page to Firebase so products can load dynamically.

--- frontend/frontend/app/shop/page.js
+++ frontend/frontend/app/shop/page.js
@@ -1,24 +1,40 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
 import ProductCard from "../../components/ProductCard";
+import { getProducts } from "../../../backend/database/products";
 
 export default function Shop() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts()
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
+
   return (
     <main>
       <Navbar />
 
       <section>
         <h1>KuroKage Shop</h1>
-        <p>Explore the Shadow Collection</p>
 
-        <ProductCard
-          name="Shadow Raven Hoodie"
-          price="$49.99"
-        />
+        <p>
+          Explore the Shadow Collection
+        </p>
 
-        <ProductCard
-          name="KuroKage Street Tee"
-          price="$29.99"
-        />
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            name={product.name}
+            price={product.price}
+          />
+        ))}
+
       </section>
     </main>
   );
