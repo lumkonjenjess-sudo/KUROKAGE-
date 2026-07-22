6e247ad7387434e99bc127af11a2bd95f781e866
lumkonjenjess-sudo <lumkonjenjess@gmail.com>
2026-07-22 20:01:44 +0200

    Update page.js
    Connects the navigation bar and creates the first KuroKage homepage layout.

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,15 @@
+import Navbar from "../components/Navbar";
+
 export default function Home() {
   return (
     <main>
-      <h1>KuroKage</h1>
-      <p>Wear the Shadow</p>
-      <p>Anime × Streetwear × Fashion</p>
+      <Navbar />
+
+      <section>
+        <h1>KuroKage</h1>
+        <h2>Wear the Shadow</h2>
+        <p>Anime × Streetwear × Fashion</p>
+      </section>
     </main>
   );
 }
