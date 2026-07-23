ea69462e2ca41e9afe973e549928c55a88d280cb
lumkonjenjess-sudo <lumkonjenjess@gmail.com>
2026-07-23 07:56:19 +0200

    Create firebase.js
    Connects KuroKage to the live Firebase project configuration.

---
+++ backend/backend/firebase/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3NHe3mhZ0kOxs5H7vYaBQSo2yqjLct8g",
+  authDomain: "kurokage-b100b.firebaseapp.com",
+  projectId: "kurokage-b100b",
+  storageBucket: "kurokage-b100b.firebasestorage.app",
+  messagingSenderId: "437240082870",
+  appId: "1:437240082870:web:4c6cc15f7b3306b48c5117",
+  measurementId: "G-RN71MV2EGJ"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
