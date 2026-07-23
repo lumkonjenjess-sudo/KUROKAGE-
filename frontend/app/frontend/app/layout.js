99dda955f4069a541538c2cb1994f7e7ccd423bd
lumkonjenjess-sudo <lumkonjenjess@gmail.com>
2026-07-23 09:41:31 +0200

    Update layout.js
    Connects the KuroKage shopping cart system to the entire application layout.

--- frontend/app/frontend/app/layout.js
+++ frontend/app/frontend/app/layout.js
@@ -1,15 +1,18 @@
+import { CartProvider } from "../context/CartContext";
 import "./globals.css";
 
 export const metadata = {
   title: "KuroKage",
-  description: "Anime × Streetwear × Fashion",
+  description: "Wear the Shadow"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {children}
+        <CartProvider>
+          {children}
+        </CartProvider>
       </body>
     </html>
   );
